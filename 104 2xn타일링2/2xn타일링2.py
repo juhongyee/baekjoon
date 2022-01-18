5cc7ff6c73680dbf9725759dde7c020eb0fc17af
unknown <dlswjdghwjd12@ajou.ac.kr>
2022-01-18 20:05:18 +0900

    dp

---
+++ 104 2xn타일링2/2xn타일링2.py
@@ -0,0 +1,19 @@
+n = int(input())
+#n번째 타일링을 구할것
+
+A = list(range(n+1))
+#n까지의 list
+
+#1,2인 경우
+A[1] = 1
+if(n>=2):
+    A[2] = 3
+
+if(n<=2):
+    print(A[n])
+else:
+    #새로운 타일이 들어갈 때 2가지 경우만 있을 수 있음.
+    for i in range(3,n+1):
+        A[i] = A[i-1]+2*A[i-2]
+
+    print(A[n]%10007)
