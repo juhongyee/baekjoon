7e135fc8d93e9734afeb3ae99883f51514bc1ba7
unknown <dlswjdghwjd12@ajou.ac.kr>
2022-01-21 01:11:26 +0900

    다이나믹프로그래밍

---
+++ 109 연속합/연속합.py
@@ -0,0 +1,22 @@
+import sys
+
+#수 개수 입력
+n = int(input())
+
+#수 저장 리스트
+A = list(map(int,sys.stdin.readline().split()))
+
+max = A[0]
+temp = 0
+for i in range(0,n):
+    if(temp + A[i]<0):
+        temp = 0
+        if(temp+A[i]>max):
+            max = temp+A[i]
+        continue
+    else:
+        if(max<temp+A[i]):
+            max = temp+A[i]
+        temp += A[i]
+print(max)
+            
